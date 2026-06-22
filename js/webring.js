a45b8077b0b3b944a088fb527f568522d891445f
chaeynz <chaeynz4msg@gmail.com>
2026-06-22 22:11:24 +0200

    oops

--- js/webring.js
+++ js/webring.js
@@ -1,6 +1,6 @@
 // Webring widget: fetches the canonical member list and wires up the
 // Previous / Random / Next links relative to whichever site is showing it.
-const WEBRING_SRC = "http://kernel.gay/webring/members.json";
+const WEBRING_SRC = "https://kernel.gay/webring/members.json";
 
 // Normalize a host so "www.foo.com" and "foo.com" compare equal.
 function normalizeHost(host) {
