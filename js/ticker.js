ce9fe557cf6d087fda98af30cddc6be4be8af25c
chaeynz <chaeynz4msg@gmail.com>
2026-06-25 00:43:59 +0200

    remove schizophrenia

--- js/ticker.js
+++ js/ticker.js
@@ -1,19 +1,15 @@
-function initTicker(span) {
-  const messages = JSON.parse(span.dataset.messages);
-  let i = 0;
-  span.textContent = messages[0];
-  span.addEventListener("animationiteration", () => {
-    i = (i + 1) % messages.length;
-    span.textContent = messages[i];
-  });
-  span.dataset.tickerInit = "true";
-}
+function change_string(ele) {
+  let idk = [
+    '100% discount on all HTTP traffic!!!',
+    'free IPv4 prefixes!',
+    'FUCK YOU',
+    'www.eff.org/nsa-spying/timeline',
+    'click here to win a cisco ASA 5506-X',
+    'you are visitor number -1',
+    'HOT SINGLES in 100.64.0.0/10 want to PING you'
+  ]
+  let i = Math.floor(Math.random() * idk.length);
+  let content = idk[i];
 
-function initTickers() {
-  document
-    .querySelectorAll(".ticker span:not([data-ticker-init])")
-    .forEach(initTicker);
+  ele.innerHTML = content;
 }
-
-initTickers();
-document.body.addEventListener("htmx:load", initTickers);
